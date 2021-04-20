4124cc3ff222b96520316b7703fabee95cce5a19
Anthony Casey <anthony.casey@defra.gov.uk>
2021-04-20 16:15:43 +0100

    Service banner change

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'The name of the service',
+  serviceName: 'Natural Flood Management',
 
   // Default port that prototype runs on
   port: '3000',
